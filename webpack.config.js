54392d354befb9152abe92204bdac5a3024f4d0d
Eugene <evgeniy.juke@gmail.com>
2019-01-09 23:51:49 +0300

    Migrate to webpack 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,35 @@
-var webpack = require("webpack"),
-		path = require("path"),
-		ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require("path");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-	context: path.join(__dirname, 'app'),
-	//точки входа
+	context: path.join(__dirname, "app"),
 	entry: "./index",
 	resolve: {
-		extensions: ['', '.js']
+		extensions: [".js"]
 	},
-	devtool: '#cheap-module-source-map',
-	//вывод	
 	output: {
-		path: path.join(__dirname, 'build'),
+		path: path.join(__dirname, "build"),
+		filename: "[name].js",
 		publicPath: "https://ejuke.github.io/cg-7th-semester/"
 	},
 	module: {
-		//загрузчики
-		loaders: [
+		rules: [
 			{
 				test: /\.css$/,
-				loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+				loader: ExtractTextPlugin.extract({
+					fallback: "style-loader",
+					use: "css-loader",
+				})
 			},
 			{
 				test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g|\.gif$/,
-				loader: 'file-loader'
+				loader: "file-loader"
 			}
 		]
 	},
-	sassLoader: {
-		outputStyle: 'compressed'
-	},
 	plugins: [
-		new ExtractTextPlugin('css/style.css', {
+		new ExtractTextPlugin("css/style.css", {
 			allChunks: true
 		})
-	]
+	],
 };
